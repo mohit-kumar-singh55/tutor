4c817806fdab459b58980622f0ff633d5e605389
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-31 20:11:28 +0530

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
     './icons/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
