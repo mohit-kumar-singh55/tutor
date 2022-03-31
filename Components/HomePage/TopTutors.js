1727659ea128eb9a0a65aa44263cbe7743875bff
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-01 00:22:16 +0530

    Using own function and Throttle worked Some How

--- Components/HomePage/TopTutors.js
+++ Components/HomePage/TopTutors.js
@@ -4,41 +4,76 @@ import { useScrollPosition } from "../../Hook/UseScrollPosition";
 
 function TopTutors() {
   const scrollRef = useRef(null);
-  const [curPos, setCurPos] = useState(null);
-  const [prevPos, setPrevPos] = useState(null);
+  // const [curPos, setCurPos] = useState(null);
+  // const [prevPos, setPrevPos] = useState(null);
+  let oldScroll = 0;
+  let newScroll = 0;
 
-  function handleScroll() {
-    if (curPos > prevPos) {
-      window.scrollBy(0, 550);
-      console.log("yes1");
-      setCurPos(null);
-      setPrevPos(null);
+  // function handleScroll() {
+  //   if (curPos > prevPos) {
+  //     window.scrollBy(0, 550);
+  //     console.log("yes1");
+  //     setCurPos(null);
+  //     setPrevPos(null);
+  //   }
+  //   else if (prevPos > curPos) {
+  //     window.scrollBy(0, -550);
+  //     console.log("yes2");
+  //     setCurPos(null);
+  //     setPrevPos(null);
+  //   }
+
+  //   console.log("No");
+  // }
+
+
+  // console.log(curPos, prevPos);
+  const handleScroll = () => {
+    newScroll = window.pageYOffset;
+    if (oldScroll < newScroll) {
+      console.log("Up");
+      window.scrollBy(0, 600);
+      // oldScroll = 0;
+      // newScroll = 0;
     }
-    else if (prevPos > curPos) {
-      window.scrollBy(0, -550);
-      console.log("yes2");
-      setCurPos(null);
-      setPrevPos(null);
+    else if (oldScroll > newScroll) {
+      console.log("Down");
+      window.scrollBy(0, -600);
     }
 
-    console.log("No");
+    oldScroll = newScroll;
   }
 
-  console.log(curPos, prevPos);
+  const throttle = (fn, limit) => {
+    let flag = true;
 
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
+    return function () {              // if any arguments also passed
+      if (flag) {
+        fn.apply(this);                         // if any arguments also passed
+        flag = false;
+
+        setTimeout(() => {
+          flag = true;
+        }, limit)
+      }
+    }
+  }
+
+  const betterExpensive = throttle(handleScroll, 2000);
 
 
-  //   return () => window.removeEventListener('scroll', handleScroll)
-  // }, [])
+  useEffect(() => {
+    window.addEventListener('scroll', betterExpensive)
 
-  useScrollPosition(({ prevPos, currPos }) => {
-    const isUp = currPos.y < prevPos.y
-    setCurPos(currPos.y);
-    setPrevPos(prevPos.y);
-    handleScroll();
-  }, [curPos, prevPos], null, true, 500)
+    return () => window.removeEventListener('scroll',handleScroll);
+  }, [newScroll, oldScroll])
+
+  // useScrollPosition(({ prevPos, currPos }) => {
+  //   const isUp = currPos.y < prevPos.y
+  //   setCurPos(currPos.y);
+  //   setPrevPos(prevPos.y);
+  //   handleScroll();
+  // }, [curPos, prevPos], null, true, 500)
 
   return (
     <main ref={scrollRef}
