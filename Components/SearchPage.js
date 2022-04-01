7ec55f6ee24ee2e4159f37fd95a6ca8f03e26613
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-01 15:25:17 +0530

    working somehow

--- Components/SearchPage.js
+++ Components/SearchPage.js
@@ -1,58 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import CourseCart from './CourseCart';
 
 function SearchPage() {
-  const scrollRef = useRef(null);
-  let oldScroll = 0;
-  let newScroll = 0;
-
-
-  // Scrolling up and down on 'scroll' event
-  const handleScroll = () => {
-    newScroll = window.pageYOffset;
-
-    if (oldScroll < newScroll) {
-      console.log("Up");
-      window.scrollBy(0, 600);                  // 600px in y Up
-    }
-    else if (oldScroll > newScroll) {
-      console.log("Down");
-      window.scrollBy(0, -550);                 // 550px in y Down
-    }
-
-    oldScroll = newScroll;
-  }
-
-  // throttled handleScroll so that it don't keep scrolling
-  const throttle = (fn, limit) => {
-    let flag = true;
-
-    return function () {              // if any arguments also passed
-      if (flag) {
-        fn.apply(this);                         // if any arguments also passed
-        flag = false;
-
-        setTimeout(() => {
-          flag = true;
-        }, limit)
-      }
-    }
-  }
-
-  const throttledHandleScroll = throttle(handleScroll, 2000);
-
-  // on which scrolling will start
-  useEffect(() => {
-    window.addEventListener('scroll', throttledHandleScroll)
-
-    return () => window.removeEventListener('scroll', throttledHandleScroll);
-  }, [newScroll, oldScroll, throttledHandleScroll])
 
 
   return (
-    <main ref={scrollRef}
-      className="flex snap-y snap-mandatory overflow-y-auto w-full max-w-[calc(1440px-250px)] items-center justify-evenly gap-y-[6rem] gap-x-4 py-11 lg:justify-around flex-wrap mx-auto">
-      {Array.from(Array(9), (_, index) => index + 1).map((index) => (
+    <main
+      className="flex h-[calc(100vh-79px)] snap-y snap-mandatory overflow-scroll overflow-x-hidden scroll-smooth transition delay-150 duration-1000 ease-in-out w-full max-w-[calc(1440px-200px)] items-center justify-evenly gap-y-[6rem] gap-x-4 py-11 lg:justify-around flex-wrap mx-auto">
+      {Array.from(Array(15), (_, index) => index + 1).map((index) => (
         <CourseCart
           key={index}
           href={'/tutors'}
