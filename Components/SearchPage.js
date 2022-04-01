e5bae43011a5d38ae74b3ab99dac94e89e74fd43
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-01 15:50:10 +0530

    Yo

--- Components/SearchPage.js
+++ Components/SearchPage.js
@@ -4,7 +4,7 @@ import CourseCart from './CourseCart';
 function SearchPage() {
   return (
     <main
-      className="flex h-[calc(100vh-79px)] snap-y snap-mandatory overflow-scroll overflow-x-hidden scroll-smooth transition delay-150 duration-1000 ease-in-out w-full max-w-[calc(1440px-180px)] items-center justify-evenly gap-y-[6rem] gap-x-2  py-11 lg:justify-around flex-wrap mx-auto">
+      className="flex h-[calc(100vh-79px)] snap-y snap-mandatory overflow-scroll overflow-x-hidden scroll-smooth transition delay-150 duration-1000 ease-in-out w-full max-w-[calc(1440px-160px)] items-center justify-evenly gap-y-[6rem] gap-x-2  py-11 lg:justify-around flex-wrap mx-auto">
       {Array.from(Array(15), (_, index) => index + 1).map((index) => (
         <CourseCart
           key={index}
